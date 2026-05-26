ded485bf836d7bc8b7591dc16fe0e2f46afd3df6
Juan Gomez <juan14gomezl@gmail.com>
2026-05-26 18:26:03 -0500

    conexion a Postgres

--- backend/java/src/main/java/com/lavanderia/java/Entity/InventoryItem.java
+++ backend/java/src/main/java/com/lavanderia/java/Entity/InventoryItem.java
@@ -2,6 +2,12 @@ package com.lavanderia.java.Entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
